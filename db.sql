f272f1734b6b8fec8766ea4df3cc557f70be846e
Илья Кабанов <kabanow.g@gmail.com>
2024-01-10 00:18:36 +0500

    fix db

--- db.sql
+++ db.sql
@@ -1,22 +1,22 @@
 CREATE TABLE users (
-    uuid UUID PRIMARY KEY,
+    uuid text PRIMARY KEY,
     username TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
 
 CREATE TABLE posts (
-    uuid UUID PRIMARY KEY,
-    author_uuid UUID,
+    uuid text PRIMARY KEY,
+    author_uuid text,
     title TEXT NOT NULL,
     text TEXT NOT NULL,
     FOREIGN KEY(author_uuid) REFERENCES users(uuid)
 );
 
 CREATE TABLE comments (
-    uuid UUID PRIMARY KEY,
-    post_uuid UUID,
-    author_uuid UUID,
+    uuid text PRIMARY KEY,
+    post_uuid text,
+    author_uuid text,
     text TEXT NOT NULL,
     FOREIGN KEY(author_uuid) REFERENCES users(uuid)
     FOREIGN KEY(post_uuid) REFERENCES posts(uuid)
